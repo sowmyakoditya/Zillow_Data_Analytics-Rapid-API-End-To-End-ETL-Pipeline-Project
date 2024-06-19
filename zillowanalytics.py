e8d70fcf0d518c100ce0959d4e27a4644f8767bb
Sowmya <166762015+sowmyakoditya@users.noreply.github.com>
2024-06-19 15:46:45 -0700

    Update zillowanalytics.py

--- zillowanalytics.py
+++ zillowanalytics.py
@@ -7,7 +7,6 @@ from airflow.operators.bash_operator import BashOperator
 from airflow.providers.amazon.aws.sensors.s3 import S3KeySensor
 from airflow.providers.amazon.aws.transfers.s3_to_redshift import S3ToRedshiftOperator
 
-
 # Load JSON config file
 with open('/home/ubuntu/airflow/config_api.json', 'r') as config_file:
     api_host_key = json.load(config_file)
@@ -16,7 +15,7 @@ now = datetime.now()
 dt_now_string = now.strftime("%d%m%Y%H%M%S")
 
 # Define the S3 bucket
-s3_bucket = 'cleaned-data-zone-csv-bucket'
+s3_bucket = 'cleaned-csv-bucket'
 
 def extract_zillow_data(**kwargs):
     url = kwargs['url']
@@ -34,23 +33,22 @@ def extract_zillow_data(**kwargs):
 
     # Write the JSON response to a file
     with open(output_file_path, "w") as output_file:
-        json.dump(response_data, output_file, indent=4)  # indent for pretty formatting
+        json.dump(response_data, output_file, indent=4)  
     output_list = [output_file_path, file_str]
     return output_list   
 
 default_args = {
     'owner': 'airflow',
     'depends_on_past': False,
-    'start_date': datetime(2023, 8, 1),
-    'email': ['myemail@domain.com'],
+    'start_date': datetime(2024, 6, 15),
+    'email': ['sowmyaks968@gmail.com'],
     'email_on_failure': False,
     'email_on_retry': False,
     'retries': 2,
     'retry_delay': timedelta(seconds=15)
 }
 
-
-with DAG('zillow_analytics_dag',
+with DAG('zillow_dataanalytics_dag',
         default_args=default_args,
         schedule_interval = '@daily',
         catchup=False) as dag:
@@ -61,9 +59,9 @@ with DAG('zillow_analytics_dag',
         op_kwargs={'url': 'https://zillow56.p.rapidapi.com/search', 'querystring': {"location":"houston, tx"}, 'headers': api_host_key, 'date_string':dt_now_string}
         )
 
-        load_to_s3 = BashOperator(
+        load_to_s3_bucket = BashOperator(
             task_id = 'tsk_load_to_s3',
-            bash_command = 'aws s3 mv {{ ti.xcom_pull("tsk_extract_zillow_data_var")[0]}} s3://endtoendyoutube-ym-bucket/',
+            bash_command = 'aws s3 mv {{ ti.xcom_pull("tsk_extract_zillow_data_var")[0]}} s3://landing-bucket/',
         )
 
         is_file_in_s3_available = S3KeySensor(
@@ -71,12 +69,12 @@ with DAG('zillow_analytics_dag',
         bucket_key='{{ti.xcom_pull("tsk_extract_zillow_data_var")[1]}}',
         bucket_name=s3_bucket,
         aws_conn_id='aws_s3_conn',
-        wildcard_match=False,  # Set this to True if you want to use wildcards in the prefix
-        timeout=60,  # Optional: Timeout for the sensor (in seconds)
-        poke_interval=5,  # Optional: Time interval between S3 checks (in seconds)
+        wildcard_match=False,  
+        timeout=60,  
+        poke_interval=5,  
         )
         
-        transfer_s3_to_redshift = S3ToRedshiftOperator(
+        transfer_s3 data_to_redshift = S3ToRedshiftOperator(
         task_id="tsk_transfer_s3_to_redshift",
         aws_conn_id='aws_s3_conn',
         redshift_conn_id='conn_id_redshift',
@@ -90,4 +88,4 @@ with DAG('zillow_analytics_dag',
 
 
 
-        extract_zillow_data_var >> load_to_s3 >> is_file_in_s3_available >> transfer_s3_to_redshift
+        extract_zillow_data_var >> load_to_s3_bucket >> is_file_in_s3_available >> transfer_s3 data_to_redshift
