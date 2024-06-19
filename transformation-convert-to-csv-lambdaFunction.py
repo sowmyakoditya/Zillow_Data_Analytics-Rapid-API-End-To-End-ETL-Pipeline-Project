b37935c5e94c9bcd5c943239f7c8fb28ebfdc8ae
Sowmya <166762015+sowmyakoditya@users.noreply.github.com>
2024-06-19 15:49:13 -0700

    Update transformation-convert-to-csv-lambdaFunction.py

--- transformation-convert-to-csv-lambdaFunction.py
+++ transformation-convert-to-csv-lambdaFunction.py
@@ -5,11 +5,10 @@ import pandas as pd
 s3_client = boto3.client('s3')
 
 def lambda_handler(event, context):
-    # TODO implement
     source_bucket = event['Records'][0]['s3']['bucket']['name']
     object_key = event['Records'][0]['s3']['object']['key']
     
-    target_bucket = 'cleaned-data-zone-csv-bucket'
+    target_bucket = 'cleaned-csv-bucket'
     target_file_name = object_key[:-5]
     print(target_file_name)
    
