f3a698723a69c3940ce73bc9296dbd80d0d0f45c
Sowmya <166762015+sowmyakoditya@users.noreply.github.com>
2024-06-19 15:36:19 -0700

    Update copyRawJsonFile-lambdaFunction.py

--- copyRawJsonFile-lambdaFunction.py
+++ copyRawJsonFile-lambdaFunction.py
@@ -4,7 +4,6 @@ import json
 s3_client = boto3.client('s3')
 
 def lambda_handler(event, context):
-    # TODO implement
     source_bucket = event['Records'][0]['s3']['bucket']['name']
     object_key = event['Records'][0]['s3']['object']['key']
    
